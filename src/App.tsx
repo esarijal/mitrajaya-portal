ce10783c42b5df6313c07b1cb40754868ff7b643
Pusaka Manggala <pusakamanggala@gmail.com>
2024-06-30 09:55:33 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Badge } from "./components/ui/badge";
+
+function App() {
+  return (
+    <>
+      <Badge>tes</Badge>
+    </>
+  );
+}
+
+export default App;
