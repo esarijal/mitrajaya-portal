f4345a1b8f78d80d3d6264a9072b49fd3ff6526f
Pusaka Manggala <pusakamanggala@gmail.com>
2024-07-02 17:03:17 +0700

    fix : bug bg color

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,19 +1,19 @@
 import Footer from "@/components/custom/footer";
 import Navbar from "@/components/custom/navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useMemo } from "react";
 
 function Layout() {
-  // get link
-  const url = window.location;
+  const location = useLocation();
+
+  const darkBg = useMemo(() => {
+    return !location.pathname.includes("/detail-berita");
+  }, [location.pathname]);
 
   return (
     <div className="bg-[#1E232E]">
       <Navbar />
-      <main
-        className={`min-h-screen ${
-          url.pathname.includes("/detail-berita") ? "bg-white" : "bg-[#1E232E] "
-        }`}
-      >
+      <main className={`min-h-screen ${darkBg ? "bg-[#1E232E]" : "bg-white"}`}>
         <div className="container pb-[90px]">
           <Outlet />
         </div>
