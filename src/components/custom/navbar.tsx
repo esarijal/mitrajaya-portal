86c8cdcbb216f6d54eb271cdad957e3de41a5aff
Pusaka Manggala <pusakamanggala@gmail.com>
2024-07-03 15:04:27 +0700

    style : width

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header className="bg-[#1E232E] sticky top-0 z-50">
       <div className="container backdrop-blur-sm">
         <div className="py-4 md:py-6">
-          <Link to="/" className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2 w-fit">
             <img src="/logo-1.png" alt="" className="h-5 md:h-12" />
             <h1 className="flex flex-col text-lg font-bold leading-tight uppercase text-start text-[#3a70bb]">
               Mitra Jaya{" "}
