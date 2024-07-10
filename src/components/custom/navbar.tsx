7226a6202c3fd1804f5616083fdeddf45441c5d0
Esa Rijal <esa.rijal@gmail.com>
2024-07-10 20:23:12 +0700

    change logo and title

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -6,12 +6,8 @@ function Navbar() {
       <div className="container backdrop-blur-sm">
         <div className="py-4 md:py-6">
           <Link to="/" className="flex items-center gap-2 w-fit">
-            <img src="/logo-1.png" alt="" className="h-5 md:h-12" />
             <h1 className="flex flex-col text-lg font-bold leading-tight uppercase text-start text-[#eab723]">
-              Mitra Jaya{" "}
-              <span className="text-xs font-semibold text-[#e4cc4c]">
-                PORTAL
-              </span>
+              B E D A S U D U T P A N D A N G . I D
             </h1>
           </Link>
           {/* <nav>Links or other content</nav> */}
